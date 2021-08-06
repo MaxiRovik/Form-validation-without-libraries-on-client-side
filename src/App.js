83d8c6f05ab340ad85740de888db5200a7765b83
Max <maxirovik@gmail.com>
2021-08-06 19:25:42 +0300

    submit button disabled whenform has some errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {useState, useEffect} from "react";
 
-function App() {
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const[emailTouch, setEmailTouch] = useState(false);
+  const[passwordTouch, setPasswordTouch] = useState(false);
+
+  const[emailError, setEmailError]=useState('field can not be empty')
+  const[passwordError, setPasswordError]=useState('field can not be empty')
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(()=>{
+    if( emailError || passwordError){
+        setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+},[emailError, passwordError])
+
+  const blurHandler = (e) => {
+    switch(e.target.name){
+        case 'email':
+            setEmailTouch(true);
+            break;
+        case 'password':
+          setPasswordTouch(true);
+            break
+    }
+};
+  const emailHandler = (e)=> {
+      setEmail(e.target.value)
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(String(e.target.value).toLowerCase())) {
+          setEmailError('incorrect email')
+
+      } else {
+          setEmailError('')
+      };
+  };
+
+    const passwordHandler = (e)=> {
+        setPassword(e.target.value)
+        if(e.target.value.length <3 || e.target.value.length>10){
+            setPasswordError('password must be from 3 to 20 symbols')
+            if(!e.target.value) {
+                setPasswordError(' field can not be empty')
+            }
+        }
+        else{
+            setPasswordError('')
+        }
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <h1 className="title">Registration</h1>
+
+        <div className="field">
+            {(emailTouch && emailError ) &&
+             <div style={{color:'red'}}>
+               {emailError}
+             </div>}
+             <label for="email">Your email</label>
+            <input value ={email} onChange = {(e) => emailHandler(e)}
+                   onBlur={e=>blurHandler(e)}
+                   name="email" type="text"
+                   placeholder="Email" className="input"/>
+        </div>
+        <div className="field">
+            {(passwordTouch && passwordError ) &&
+            <div style={{color:'red'}}>
+                {passwordError}
+            </div>}
+            <label for="password">Enter your password</label>
+            <input value = {password} onChange = {(e) => passwordHandler(e)}
+                   onBlur={e=>blurHandler(e)}
+                   name ="password" type="password"
+                   placeholder="password" className="input"/>
+        </div>
+        <button disabled ={!formValid} type = "submit" className="btn">Confirm</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
